cf24aef6801ae2448bdda708828fc9a560e372fb
Matthew Staples <staplma2@uw.edu>
2017-05-22 14:16:18 -0700

    added images, html and js scaffolding

---
+++ app.js
@@ -0,0 +1,7 @@
+var images_arr = [];
+// this has the three just chosen, replaced each time
+var justShown = [];
+var globalClicks = 0;
+var img1 = document.getElementById('img-one');
+var img2 = document.getElementById('img-two');
+var img3 = document.getElementById('img-three');
