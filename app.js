9480cce1eec896407d6a061d1c2bf421debda883
Matthew Staples <staplma2@uw.edu>
2017-05-22 15:52:31 -0700

    render works

--- app.js
+++ app.js
@@ -14,26 +14,26 @@ function Img(name, filePath) {
   images_arr.push(this);
 }
 
-var bag = new Img('bag', './img/bag.jpg');
-var banana = new Img('banana', './img/banana.jpg');
-var bathroom = new Img('bathroom', './img/bathroom.jpg');
-var boots = new Img('boots', './img/boots.jpg');
-var breakfast = new Img('breakfast', './img/breakfast.jpg');
-var bibblegum = new Img('bubblegum', './img/bubblegum.jpg');
-var chair = new Img('chair', './img/chair.jpg');
-var cthulu = new Img('cthulu', './img/cthulu.jpg');
-var dogDuck = new Img('dogDuck', './img/dog-duck.jpg');
-var dragon = new Img('dragon', './img/dragon.jpg');
-var pen = new Img('pen', './img/pen.jpg');
-var petSweep = new Img('petSweep', './img/pet-sweep.jpg');
-var scissors = new Img('scissors', './img/scissors.jpg');
-var shark = new Img('shark', './img/shark.jpg');
-var sweep = new Img('sweep', './img/sweep.png');
-var tauntaun = new Img('tauntaun', './img/tauntaun.jpg');
-var unicorn = new Img('unicorn', './img/unicorn.jpg');
-var usb = new Img('usb', './img/usb.gif');
-var waterCan = new Img('waterCan', './img/water-can.jpg');
-var wine = new Img('wineGlass', './img/wine-glass.jpg');
+var bag = new Img('bag', 'img/bag.jpg');
+var banana = new Img('banana', 'img/banana.jpg');
+var bathroom = new Img('bathroom', 'img/bathroom.jpg');
+var boots = new Img('boots', 'img/boots.jpg');
+var breakfast = new Img('breakfast', 'img/breakfast.jpg');
+var bibblegum = new Img('bubblegum', 'img/bubblegum.jpg');
+var chair = new Img('chair', 'img/chair.jpg');
+var cthulhu = new Img('cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new Img('dogDuck', 'img/dog-duck.jpg');
+var dragon = new Img('dragon', 'img/dragon.jpg');
+var pen = new Img('pen', 'img/pen.jpg');
+var petSweep = new Img('petSweep', 'img/pet-sweep.jpg');
+var scissors = new Img('scissors', 'img/scissors.jpg');
+var shark = new Img('shark', 'img/shark.jpg');
+var sweep = new Img('sweep', 'img/sweep.png');
+var tauntaun = new Img('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Img('unicorn', 'img/unicorn.jpg');
+var usb = new Img('usb', 'img/usb.gif');
+var waterCan = new Img('waterCan', 'img/water-can.jpg');
+var wine = new Img('wineGlass', 'img/wine-glass.jpg');
 
 //console.log(images_arr)
 
@@ -64,8 +64,9 @@ function selectImgs() {
   return justShown;
 }
 
-function render(indexArr) {
-  img1.setAttibute('src', images_arr[indexArr[0]].filePath);
-  img2.setAttibute('src', images_arr[indexArr[1]].filePath);
-  img3.setAttibute('src', images_arr[indexArr[2]].filePath);
+function render() {
+  var indexArr = selectImgs();
+  img1.src =  images_arr[indexArr[0]].filePath;
+  img2.src = images_arr[indexArr[1]].filePath;
+  img3.src = images_arr[indexArr[2]].filePath;
 }
